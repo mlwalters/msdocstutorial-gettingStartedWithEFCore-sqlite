24518e1a48059050f7fa31e6a3956ca4fd15fdfb
Maricar Walters <walterskai7@gmail.com>
2021-02-02 15:31:03 -0600

    Modified Model class code

--- EFGetStarted/EFGetStarted/Model.cs
+++ EFGetStarted/EFGetStarted/Model.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Text;
 
 namespace EFGetStarted
 {
-    class Model
+    //class Model
+    //{
+    //    //https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio#create-a-new-project
+    //}
+
+    public class BloggingContext : DbContext
     {
-        //https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio#create-a-new-project
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
     }
 }
