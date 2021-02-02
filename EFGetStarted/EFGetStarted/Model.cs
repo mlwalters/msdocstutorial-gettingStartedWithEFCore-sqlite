2797a5d7e78203ddb87cc1b14319b1c1691532ab
Maricar Walters <walterskai7@gmail.com>
2021-02-02 15:24:37 -0600

    initial commit for msdocs tutorial on EF Core and sqlite

---
+++ EFGetStarted/EFGetStarted/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFGetStarted
+{
+    class Model
+    {
+        //https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio#create-a-new-project
+    }
+}
---
+++ EFGetStarted/EFGetStarted/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio#create-a-new-project
+        }
+    }
+}
