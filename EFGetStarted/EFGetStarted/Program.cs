3d7926b7bed5aacdefbd2ea95e56a6a7d892dbbe
Maricar Walters <walterskai7@gmail.com>
2021-02-11 14:28:56 -0600

    Updated database

--- EFGetStarted/EFGetStarted/Program.cs
+++ EFGetStarted/EFGetStarted/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 namespace EFGetStarted
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
-            //https://docs.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio#create-a-new-project
+            using (var db = new BloggingContext())
+            {
+                // Note: This sample requires the database to be created before running.
+
+                // Create
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+                db.SaveChanges();
+
+                // Delete
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
         }
     }
 }
